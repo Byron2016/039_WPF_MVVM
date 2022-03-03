52135eefe17bc00ba1872f064f0fa955c428712c
Byron 2016 <bgva2005@yahoo.com>
2022-03-03 10:30:03 -0500

    feat: add model Customer

---
+++ WPF_MVVM/Models/Customer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace WPF_MVVM.Models
+{
+    public class Customer
+    {
+        public int ID { get; set; }
+        public string FistName { get; set; }
+        public string SecondName { get; set; }
+        public string LastName { get; set; }
+        public bool IsEnable { get; set; }
+        public DateTime LastLogin { get; set; }
+    }
+}
