62a6297b06668b0a3ac4ecc49ebac9af6d8a849b
Byron 2016 <bgva2005@yahoo.com>
2022-03-03 10:37:08 -0500

    feat: add moke database

---
+++ WPF_MVVM/DAL/CustomerService.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using WPF_MVVM.Models;
+
+namespace WPF_MVVM.DAL
+{
+    public class CustomerService : ICustomerService
+    {
+        public List<Customer> GetCustomer()
+        {
+            var customers = new List<Customer>();
+
+            for (int i = 0; i < 20; i++)
+            {
+                customers.Add(new Customer()
+                {
+                    ID = i,
+                    FistName = Guid.NewGuid().ToString(),
+                    SecondName = Guid.NewGuid().ToString(),
+                    LastName = Guid.NewGuid().ToString(),
+                    IsEnable = i > 10,
+                    LastLogin = DateTime.Now.AddDays(i)
+                });
+            }
+
+            return customers;
+        }
+
+    }
+}
---
+++ WPF_MVVM/DAL/ICustomerService.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using WPF_MVVM.Models;
+
+namespace WPF_MVVM.DAL
+{
+    public interface ICustomerService
+    {
+        public List<Customer> GetCustomer();
+    }
+}
