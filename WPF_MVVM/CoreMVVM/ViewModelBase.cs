270fe86136ca9d4bb4826b79a5a8c3904e3627be
Byron 2016 <bgva2005@yahoo.com>
2022-03-03 10:34:21 -0500

    feat: add CoreMVVM

---
+++ WPF_MVVM/CoreMVVM/ViewModelBase.cs
@@ -0,0 +1,38 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace WPF_MVVM.CoreMVVM
+{
+    public abstract class ViewModelBase : INotifyPropertyChanging, INotifyPropertyChanged
+    {
+        public event PropertyChangingEventHandler? PropertyChanging;
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        protected void OnPropertyChanging([CallerMemberName] string propertyName = "")
+        {
+            //if (PropertyChanging != null)
+            //{
+            //    PropertyChanging(this, new PropertyChangingEventArgs(propertyName));
+            //}
+            //PropertyChanging?.Invoke(this, new PropertyChangingEventArgs(propertyName));
+
+            PropertyChanging(this, new PropertyChangingEventArgs(propertyName));
+
+        }
+
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            //if(PropertyChanged != null)
+            //{
+            //    //PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            //    PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            //}
+            //PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
